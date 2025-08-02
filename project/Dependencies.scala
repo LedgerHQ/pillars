0892b63fa3689d82916196888c694940c708534b
Scala Steward <scala-steward@ledger.com>
2025-08-02 00:44:53 +0000

    chore: Update cats-effect from 3.6.0 to 3.6.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object versions {
     val scala            = "3.6.4"
     // Dependencies
     val cats             = "2.13.0"
-    val catsEffect       = "3.6.0"
+    val catsEffect       = "3.6.3"
     val circe            = "0.14.14"
     val circeYaml        = "0.16.1"
     val decline          = "2.5.0"
