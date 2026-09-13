906e72fa73a2dbc058c06c2d16baf8bfa2a0ac30
Scala Steward <scala-steward@ledger.com>
2026-09-13 00:36:45 +0000

    chore: Update munit-cats-effect from 2.1.0 to 2.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -26,7 +26,7 @@ object versions {
 
     object munit {
         val core       = "1.2.4"
-        val catsEffect = "2.1.0"
+        val catsEffect = "2.2.0"
         val scalacheck = "1.3.1"
         val http4s     = "1.1.0"
     }
