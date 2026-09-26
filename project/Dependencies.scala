ba1823d5c93c945b966cfc282382f5111d6ca078
Scala Steward <scala-steward@ledger.com>
2026-09-26 00:45:02 +0000

    chore: Update scribe from 3.17.0 to 3.19.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object versions {
     val otel4s           = "0.13.2"
     val postgresqlDriver = "42.7.8"
     val rediculous       = "0.5.1"
-    val scribe           = "3.17.0"
+    val scribe           = "3.19.0"
     val skunk            = "1.0.0-M11"
     val tapir            = "1.11.50"
     val testContainers   = "0.43.6"
